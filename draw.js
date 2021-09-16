d2f6f22e2fe1196e0f74477d89a751895435a907
olibaron <olivier@airwave,io>
2021-09-16 13:11:25 +0200

    First commit

---
+++ draw.js
@@ -0,0 +1,31 @@
+let finalResult;
+
+const numberOfDraws = 5;
+const numberOfContestants = 104;
+
+// Punks that are in this draw
+const xDaiPunkArray = ['#592', '#1447', '#1766', '#8709', '#8773'];
+
+const drawArray = Array.from({ length: numberOfContestants }, (v, k) => k);
+const randomArray = shuffleArray(drawArray);
+const randomPunkArray = shuffleArray(xDaiPunkArray);
+
+const drawResult = randomArray.slice(0, numberOfDraws);
+
+finalResult = [];
+for(let i = 0, iCount = drawResult.length; i < iCount; i++) {
+    finalResult.push({index: drawResult[i], awardedPunk: randomPunkArray[i]});
+}
+
+console.log('In xDaiPunk style, The winners are', finalResult);
+console.log('Done');
+
+// Randomize array in-place using Durstenfeld shuffle algorithm
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+}
